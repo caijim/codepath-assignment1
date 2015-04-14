61f126197e2a0e45c4ff97d0076e72a9b38d415f
Jim <caijim@github.com>
2015-04-13 18:16:24 -0700

    first commit

---
+++ tomoatoes/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  tomoatoes
+//
+//  Created by Jim Cai on 4/7/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ tomoatoes/MovieCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieCollectionViewCell.swift
+//  tomoatoes
+//
+//  Created by Jim Cai on 4/13/15.
+//  Copyright (c) 2015 com.codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+}
